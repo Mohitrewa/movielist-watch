bcf4f6a88da9850b8386177f584ce2fed5979279
Mohitrewa <139006389+Mohitrewa@users.noreply.github.com>
2024-11-17 21:30:38 +0530

    Add files via upload

---
+++ Movie-Watchlist-master/src/App.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import "./lib/font-awesome/css/all.min.css";
+import "./App.css";
+
+import { GlobalProvider } from "./context/GlobalState"
+
+const App = () => {
+    return (
+        <GlobalProvider>
+            <Router>
+                <Header/>
+
+                <Switch>
+                    <Route exact path="/">
+                        <Watchlist/>
+                    </Route>
+
+                    <Route exact path="/watched">
+                        <Watched/>
+                    </Route>
+
+                    <Route exact path="/add">
+                        <Add/>
+                    </Route>
+                </Switch>
+            </Router>
+        </GlobalProvider>
+    )
+}
+
+export default App
